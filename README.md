f6b1933918347b70d2a05007562aeb30c3a89aac
KwatME <kwat.me@icloud.com>
2020-08-15 10:11:47 -0700

    Compare

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Match Pattern
+# Compare
 
 #
 
--- code/environment.py
+++ code/environment.py
@@ -1,9 +1,2 @@
-import itertools
-import json
-import os
 
-import numpy as np
-import pandas as pd
 
-import kraft
-from make_path_dict import make_path_dict
