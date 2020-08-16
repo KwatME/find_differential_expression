3edecd42dae665c24b97d1b259fa33882edd6e20
KwatME <kwat.me@icloud.com>
2020-08-16 09:46:53 -0700

    function_heat_map: scores_

--- code/__init__.py
+++ code/__init__.py
@@ -6,24 +6,3 @@ import pandas as pd
 import kraft
 
 SETTING = kraft.json.read("setting.json")
-
-PATH = {}
-
-for name in (
-    "gene_x_sample.processed.tsv",
-    "target_x_sample.tsv",
-    "find_differentially_expressed_gene/",
-    "gene_set_x_information.tsv",
-    "gsea/",
-    "gene_set_x_sample.tsv",
-    "find_differentially_expressed_gene_set/",
-    "compare_differentially_expressed_gene_set/",
-    "expand_gene_set/",
-    "summarize_gene_set/",
-):
-
-    path = os.path.join("../output/", name)
-
-    kraft.path.path(path)
-
-    PATH[name] = path
