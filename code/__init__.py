643d879a9a33d0b79cbd270a51a5a6f7262946ea
KwatME <kwat.me@icloud.com>
2021-06-01 13:32:36 -0700

    rg

--- code/__init__.py
+++ code/__init__.py
@@ -1,10 +1,11 @@
 import os
 import re
 
-import kraft
 import numpy as np
 import pandas as pd
 from get_log_ratio import get_log_ratio
 from plot_peek import plot_peek
+
+import kraft
 
 SETTING = kraft.json.read("setting.json")
