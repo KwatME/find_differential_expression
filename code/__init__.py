cb9004129cd8d3d107b20b0ab6046eb37cc0406d
KwatME <kwat.me@icloud.com>
2021-06-05 16:36:36 -0700

    Sync

--- code/__init__.py
+++ code/__init__.py
@@ -1,11 +1,12 @@
 import os
 import re
 
-import kraft
 import numpy as np
 import pandas as pd
 from get_difference import get_difference
 from plot_peek import plot_peek
 from read_statistic import read_statistic
+
+import kraft
 
 SETTING = kraft.json.read("setting.json")
